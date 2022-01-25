cc75d29193ee63b7e1f1e3442f237a2afecfc3ac
luka <lukajan.klepac@gmail.com>
2022-01-26 00:37:37 +0100

    removed numpy, rewrote field

--- snake.py
+++ snake.py
@@ -1,8 +1,10 @@
-import numpy
-
 def game():
-    game_layout = numpy.zeros((11,11))
-
-    print(game_layout)
+    # 7x7 playing field
+    rows, cols = (7,7)
+    field = [[0 for i in range(cols)] for j in range(rows)]
+    field[3][3] = 9
+    for row in field:
+        print(row)
+    
 
 game()

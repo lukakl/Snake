2743fa0b39afb3ace600e2bd3cfcf8249b8c1cc9
luka <lukajan.klepac@gmail.com>
2022-01-29 21:47:36 +0100

    rewrote field, added game tick + potential food creation

--- snake.py
+++ snake.py
@@ -1,10 +1,31 @@
-def game():
-    # 7x7 playing field
-    rows, cols = (7,7)
-    field = [[0 for i in range(cols)] for j in range(rows)]
-    field[3][3] = 9
-    for row in field:
-        print(row)
-    
+import os
+from random import randint
+import time
 
+def game():
+
+    rows, cols = (7, 7)
+
+    #creates 2d array
+    grid = [[0 for i in range(rows)] for j in range(cols)]
+
+    #creates 1000ms game tick using while loop
+    a = 0
+    while a <= 0:
+        
+        os.system("cls")
+
+        x = randint(0, 6)
+        y = randint(0, 6)
+        food = randint(1, 100)
+
+        #adds 1/3 chance of spawning food
+        if food < 34:
+                grid[x][y] = 1
+
+        for f in grid:
+            print(f)
+
+        time.sleep(1)
+    
 game()
